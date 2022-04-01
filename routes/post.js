edc61a922bee9c608217a47fbb9c455e84d0ff8d
Kimseonghyoek <cilo1223@naver.com>
2022-04-01 17:48:50 +0900

    이메일 중복방지

--- routes/post.js
+++ routes/post.js
@@ -6,18 +6,23 @@ const bcrypt = require("bcrypt");
 router.use(express.urlencoded());
 router.use(express.json());
 
-router.post("/post", async (req, res) => {
+router.post("/post", async (req, res, next) => {
   const {name, email} = req.body;
   let password = req.body;
   const user = new User(req.body);
   const Salt = await bcrypt.genSalt(10);
   password = await bcrypt.hash(user.password, Salt);
   try {
+    const exUser = await User.findOne({ where : { email }});
+    if(exUser) {
+      return console.error("이메일이 중복됩니다.");
+    } 
     await User.create({
       name,
       email,
       password,
     });
+    return res.redirect('/');
   } catch (err) {
     console.log(err);
   }
