ac7fce6addc96285bb620fdb812015ad9b59783c
Kimseonghyoek <cilo1223@naver.com>
2022-04-04 23:50:28 +0900

    passport 수정 및 코드추가

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const local = require('passport-local');
+const passport = require("passport");
+const local = require("passport-local");
+const User = require("../models/user");
+const app = express();
 
-router.get('/',  (req, res) => {
-  res.render('auth');
-})
-
-router.get('/signin', (req, res) => {
-  res.render('sign');
+router.get("/", (req, res) => {
+  res.render("auth");
 });
 
-router.get('/signup', (req, res) => {
-  res.render('signup');
+router.get("/signin", (req, res) => {
+  res.render("sign");
 });
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+passport.use(
+  new local((username, password, done) => {
+    // User.findOne({
+    //   username: username
+    // }, (err, user) => {
+    //   if(err) {
+    //     return done(err);
+    //   }
+    //   if(!user) {
+    //     return done(null, false, {
+    //       message: 'Incorrect username'
+    //     });
+    //   }
+    //   if(!user.validPassword(password)) {
+    //     return done(null, false, {
+    //       message: 'Incorrect password'
+    //     });
+    //   }
+    //   return done(null, user);
+    // })
+  })
+);
 
 // 전략은 로컬로 하며 성공할 경우 Hww가 나오고 실패할 경우 썅이 나온다
-router.post('/login', async(req, res, next) => {
-  try {
-    await passport.authenticate('local', {
-      successRedirect: res.send("HWW"),
-      failureRedirect: res.send("썅")
-    })
-  } catch(err) {
-    console.error(err);
-  }
-})
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: '/',
+    failureRedirect: '/auth/signin'
+  })
+);
 
 module.exports = router;
